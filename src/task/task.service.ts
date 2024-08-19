488fd5bc532073c96dbd4ebcdc93cc7699a4cc59
Leila <leila_rosa3@outlook.com>
2024-08-19 17:07:20 -0300

    first commit: post created

---
+++ src/task/task.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+    
+    private task: TaskDto[] = [];
+    
+    create(task: TaskDto) {
+        this.task.push(task);
+    }
+}
